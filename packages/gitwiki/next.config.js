41a8551368d925d5c3f01f9f91a6875e1f79cfc5
Frank Lyder Bredland <fl@nyggen.com>
2022-10-18 14:11:39 +0200

    Upgrade Next.js, React and related dependencies

--- packages/gitwiki/next.config.js
+++ packages/gitwiki/next.config.js
@@ -1,58 +1,12 @@
-const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
-const { getGlobalCssLoader } = require('next/dist/build/webpack/config/blocks/css/loaders');
-const MiniCssExtractPlugin = require('next/dist/build/webpack/plugins/mini-css-extract-plugin/src').default;
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+const withVanillaExtract = createVanillaExtractPlugin();
 
-module.exports = {
-	future: {
-		webpack5: true,
-	},
-
-  webpack: (config, { dev, isServer, ...options }) => {
+module.exports = withVanillaExtract({
+  webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
+			config.resolve.fallback.fs = false;
     }
-
-		// --- vanilla-extract config ---
-		// based on: https://github.com/seek-oss/vanilla-extract/issues/4#issuecomment-810842869
-		config.module.rules.push({
-			test: /\.css$/i,
-			sideEffects: true,
-			use: dev
-			? getGlobalCssLoader(
-				{
-					assetPrefix: options.config.assetPrefix,
-					future: {
-						webpack5: true,
-					},
-					isClient: !isServer,
-					isServer,
-					isDevelopment: dev,
-				},
-				[],
-				[]
-			)
-			: [MiniCssExtractPlugin.loader, 'css-loader'],
-		});
-
-		config.plugins.push(
-			new VanillaExtractPlugin()
-		);
-
-		if (!dev) {
-			config.plugins.push(
-				new MiniCssExtractPlugin({
-					filename: 'static/css/[contenthash].css',
-					chunkFilename: 'static/css/[contenthash].css',
-					ignoreOrder: true,
-				})
-			);
-		}
-
-		config.resolve.extensions = ['.js', '.jsx', '.ts', '.tsx', '.css', '.css.ts'];
-		// ------------------------------
 
     return config;
   },
@@ -62,4 +16,4 @@ module.exports = {
 			{ source: '/', destination: '/index', permanent: true },
 		]
 	}
-};
+});
