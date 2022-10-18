804820e4d12019be30813bb94051ce59ba096ffd
Frank Lyder Bredland <fl@nyggen.com>
2022-10-18 13:38:46 +0200

    Upgrading Storybook to verion 6.5.12

--- packages/gitwiki/.storybook/main.js
+++ packages/gitwiki/.storybook/main.js
@@ -10,6 +10,11 @@ module.exports = {
     '@storybook/addon-essentials',
 		'@storybook/addon-postcss',
   ],
+	framework: '@storybook/react',
+	core: {
+		builder: 'webpack5',
+		disableTelemetry: true,
+	},
 	webpackFinal: async (config, { configType }) => {
 
 		config.plugins.push(new VanillaExtractPlugin());
